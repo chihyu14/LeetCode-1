5a0ed939085a81a8f8bb70eae9626ced607eb5d4
klion26 <qcx978132955@gmail.com>
2013-12-13 10:34:06 +0800

    add Maximum-Subarray.cpp

--- Maximum-Subarray.cpp
+++ Maximum-Subarray.cpp
@@ -27,4 +27,73 @@ public:
     }
 };
 
-//Solution 2: divide and conquer
+//Solution 2: divide and conquers
+class Solution {
+public:
+    int maxSubArray(int A[], int n) {
+        ///use diveide and conquer
+        return divideandconquer(A, 0, n-1);
+    }
+    //divide and conquer function
+    //@A: the array
+    //@l: the leftest index
+    //@h: the rightest index
+    int divideandconquer(int A[], int l, int h)
+        {
+            //l == h, return directly
+            if(l == h)
+                return A[l];
+            //@ret: answer we will return
+            //we divide A[l,h] to two subarrays
+            //A[l,mid] && A[mid+1, h],
+            //the maximum subarray is either in the left subarray
+            //or the right subarray, or cross the mid-point
+            int ret = INT_MIN;
+            int mid = (l+h)>>1, t;
+            ///get the maximum subarray of the left subarray
+            t = divideandconquer(A,l,mid);
+            //update ret
+            if(t > ret)
+                ret = t;
+            //get the maximum subarray of the right subarray
+            t = divideandconquer(A,mid+1,h);
+            //update ret
+            if(t > ret)
+                ret = t;
+            //get the maximum subarray if it crosses the mid-point
+            t = crossmax(A, l, mid, h);
+            //update ret
+            if(t > ret)
+                ret = t;
+            return ret;
+        }
+    //get the maximum subarray of A[l,h], and, it crosses
+    //the mid-point!!!
+    int crossmax(int A[], int l, int mid, int h)
+        {
+            //the left maximum sum including A[mid]
+            int left_ret = INT_MIN;
+            int sum = 0;
+            //get the maximum subarray A[i, mid]
+            //i from mid to l
+            for(int i=mid;i>=l;--i)
+                {
+                    sum += A[i];
+                    if(left_ret < sum)
+                        left_ret = sum;
+                }
+            sum = 0;
+            //same as above
+            int right_ret = INT_MIN;
+            //get the maximum subarray A[mid+1, j]
+            //j form mid+1 to h
+            for(int i=mid+1;i <=h; ++i)
+                {
+                    sum += A[i];
+                    if(right_ret < sum)
+                        right_ret = sum;
+                }
+            //return the answer
+            return left_ret + right_ret;
+        }
+};
